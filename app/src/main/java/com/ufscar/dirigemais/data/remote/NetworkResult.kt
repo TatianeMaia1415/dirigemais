3d1755417660cd4252a92945e952c1a29fb30a40
Tatiane Maia de Oliveira <124464932+TatianeMaia1415@users.noreply.github.com>
2026-05-21 07:53:40 -0300

    Update NetworkResult.kt

--- app/src/main/java/com/ufscar/dirigemais/data/remote/NetworkResult.kt
+++ app/src/main/java/com/ufscar/dirigemais/data/remote/NetworkResult.kt
@@ -1,11 +1,6 @@
 package com.ufscar.dirigemais.data.remote
 
-/**
- * Sealed class — representa os 3 estados possíveis de uma chamada de rede.
- * É como uma caixa lacrada: só pode ser Loading, Success ou Error.
- *
- * Usado pela ViewModel para notificar a UI do estado atual.
- */
+
 sealed class NetworkResult<out T> {
     /** Requisição em andamento */
     object Loading : NetworkResult<Nothing>()
